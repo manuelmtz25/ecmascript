98d6cf2e044e0675186702496a1142c2d3a5d36a
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-09 09:37:14 -0500

    ES7 - array "includes" y potencia

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+//"includes", saber si hay un elemento dentro de un arreglo o un string
+
+let numbers = [1, 2, 3, 7, 8]
+
+if(numbers.includes(7)){
+    console.log('Si se encuentra el valor 7')
+}else{
+    console.log('No se encuentra')
+}
+
+//Potencia
+
+let base=4;
+let exponente=3;
+let result=base ** exponente
+console.log(result)
