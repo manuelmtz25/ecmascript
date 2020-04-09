d0dbd940da9f4b23fa7d6d399ab2ad13612ea558
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-09 09:25:19 -0500

    ES6 - Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -171,3 +171,41 @@ helloPromise()
   .then(response=>console.log(response))
   .then(()=>console.log('hola'))
   .catch(error=>console.log(error))
+
+//Clases
+class calculator {
+  constructor() {
+    this.valueA = 0
+    this.valueB = 0
+  }
+
+  sum(valueA, valueB){
+    this.valueA = valueA;
+    this.valueB = valueB;
+    return this.valueA + this.valueB;
+  }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2, 2))
+
+//Módulos
+import {hello} from './module';
+
+hello();
+
+//Generadores
+//función especial que retorna una serie de valores
+
+function* helloWorld(){
+  if(true){
+    yield 'Hello, ';
+  }
+  if(true){
+    yield 'World';
+  }
+}
+
+const generatorHello=helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
